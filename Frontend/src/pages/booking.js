a0b9f77472a1bc895387e58ac93d20ff0088eff5
Nikkyjung <nikkyjungalbion@gmail.com>
2023-10-09 20:01:52 +0700

    update booking.js and add booking.css

--- Frontend/src/pages/booking.js
+++ Frontend/src/pages/booking.js
@@ -1,3 +1,25 @@
+import React, { useState } from "react";
+import "./booking.css"
+
 export default function Booking() {
-    return <h1>Booking</h1>
+    const [firstdate, setFirstDate] = useState('');
+    const [enddate, setEndDate] = useState('');
+    console.log(firstdate);
+    return (
+       <div className="booking">
+        <h1>Booking</h1>
+        <p>First Date</p>
+       <div className="dateborder">
+        <input type="date" 
+        required
+        value={firstdate} onChange={(e) => setFirstDate(e.target.value)}/>
+       </div>
+       <p>End Date</p>
+       <div className="dateborder">
+        <input type="date"
+        required 
+        value={enddate} onChange={(e) => setEndDate(e.target.value)}/>
+       </div>
+       </div>
+    )
 }
