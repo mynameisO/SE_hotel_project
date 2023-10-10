daca2b09e03bacf15a8c5717ae0958033065cebc
poon <tanuthumkingkaew@gmail.com>
2023-10-10 22:49:05 +0700

    Add homepage.js and homepage.css

--- Frontend/src/pages/home.js
+++ Frontend/src/pages/home.js
@@ -1,3 +1,31 @@
-export default function Home() {
-    return <h1>Home</h1>
+import React from "react";
+import './home.css'
+import home1 from './image/home1.jpg';
+import home2 from './image/home2.jpg';
+import home3 from './image/home3.jpg';
+import home4 from './image/home4.jpg';
+export default function Home(){
+    return (
+        <body>
+            <div class = "headContainer">
+                <div class = "box">
+                    <div class = "text">
+                        <h1>Hello</h1>
+                        <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.  
+                        </p>
+                        <button type = "submit" className = "btn">More Information</button>
+                    </div>
+                    <div class = "image">
+                        <img src = {home1}  class = "slide"/>
+                    </div>
+                    <div class = "imageItem">
+                        <img src= {home2}  alt="" />
+                        <img src= {home3}  alt="" />
+                        <img src= {home4}  alt=""/>
+                    </div>
+                </div> 
+            </div>
+        </body>
+    )
 }
