d5941f6c4c613c46469bf21235ebdb9d59807251
GNOGGONG <peerawat291@gmail.com>
2023-10-19 18:09:47 +0700

    Make change of the checkRoomAvailability function to handle the situation where if a customer is booking multiple room types in one booking and you want to ensure that the booking is made only if all the requested room types and are available. also make change to the function createBooking to suit with the checkRoomAvailability function

--- GNOGBackend/insertBookingInfo.js
+++ GNOGBackend/insertBookingInfo.js
@@ -14,9 +14,9 @@ const bookingData = {
   "checkin_date": "2023-11-25",
   "checkout_date": "2023-11-27",
   "rooms": [
-    { 
-        "room_type_id": "3",
-        "num_rooms": 1
+    {
+      "room_type_id": "3",
+      "num_rooms": 1
     }
   ],
   "guest_title": "Mr",
@@ -27,15 +27,13 @@ const bookingData = {
   "guest_telnum": "0800000000",
 };
 
-function checkRoomAvailability(checkinDate, checkoutDate, rooms) {
-  return new Promise((resolve, reject) => {
-    const roomTypeIds = rooms.map(room => room.room_type_id);
-    const numRooms = rooms.map(room => room.num_rooms);
-
+async function checkRoomAvailability(checkinDate, checkoutDate, rooms) {
+  const availabilityPromises = rooms.map(async room => {
+    const { room_type_id, num_rooms } = room;
     const availabilityQuery = `
-      SELECT room_id, COUNT(*) AS booked_rooms
+      SELECT room_id, COUNT(*) AS available_rooms
       FROM room
-      WHERE room_id IN (${roomTypeIds.join(',')})
+      WHERE room_type_id = ${room_type_id}
         AND room_status = 'free'
         AND room_id NOT IN (
           SELECT room_id
@@ -45,132 +43,138 @@ function checkRoomAvailability(checkinDate, checkoutDate, rooms) {
             FROM booking
             WHERE (checkin_date BETWEEN '${checkinDate}' AND '${checkoutDate}' OR
                    checkout_date BETWEEN '${checkinDate}' AND '${checkoutDate}')
-              AND booking_status = 'confirmed'
           )
         )
-      GROUP BY room_id;
+      GROUP BY room_id
+      HAVING available_rooms >= ${num_rooms};
     `;
 
-    connection.query(availabilityQuery, (error, results, fields) => {
-      if (error) {
-        reject(error);
-      } else {
-        const availableRooms = results.reduce((acc, row) => {
-          const availableQuantity = row.room_id in numRooms ? numRooms[row.room_id] - row.booked_rooms : numRooms[row.room_id];
-          if (availableQuantity > 0) {
-            acc.push({
-              room_id: row.room_id,
-              available_quantity: availableQuantity
-            });
+    return new Promise((resolve, reject) => {
+      connection.query(availabilityQuery, (error, results) => {
+        if (error) {
+          reject(error);
+        } else {
+          if (results.length > 0) {
+            resolve(results[0].room_id);
+          } else {
+            resolve(null);
           }
-          return acc;
-        }, []);
-
-        const allRoomsAvailable = availableRooms.length === roomTypeIds.length;
-        resolve({ available: allRoomsAvailable, rooms: availableRooms });
-      }
+        }
+      });
     });
   });
+
+  try {
+    const roomIds = await Promise.all(availabilityPromises);
+    // Check if all room types are available
+    if (roomIds.every(roomId => roomId !== null)) {
+      return roomIds;
+    } else {
+      return null; // Some room types are not available
+    }
+  } catch (error) {
+    throw error;
+  }
 }
 
 
-function createBooking(bookingData) {
-  // Check room availability
-  checkRoomAvailability(bookingData.checkin_date, bookingData.checkout_date, bookingData.rooms)
-    .then(roomsAvailable => {
-      if (roomsAvailable) {
-        // Rooms are available, proceed to create booking
-        // Insert guest data
-          // Generate a unique guest_id
-        const guestId = Math.floor(Math.random() * 99999999) + 1;
-        const guestInsertQuery = `
-        INSERT INTO guest (guest_id, guest_title, guest_first_name, guest_last_name, guest_email, guest_address, guest_telnum)
-        VALUES ('${guestId}', '${bookingData.guest_title}', '${bookingData.guest_first_name}', '${bookingData.guest_last_name}', '${bookingData.guest_email}', '${bookingData.guest_address}', '${bookingData.guest_telnum}');
-      `;
+async function createBooking(bookingData) {
+  try {
+    const roomIds = await checkRoomAvailability(
+      bookingData.checkin_date,
+      bookingData.checkout_date,
+      bookingData.rooms
+    );
 
-        // Execute guest data insertion query
-        connection.query(guestInsertQuery, (guestInsertError, guestInsertResults, guestInsertFields) => {
-          if (guestInsertError) {
-            console.error('Error inserting guest data:', guestInsertError);
-          } else {
-            // Guest data inserted successfully, retrieve the guest_id
+    if (roomIds !== null) {
+      const guestId = Math.floor(Math.random() * 99999999) + 1;
+      const guestInsertQuery = `
+      INSERT INTO guest (guest_id, guest_title, guest_first_name, guest_last_name, guest_email, guest_address, guest_telnum)
+      VALUES ('${guestId}', '${bookingData.guest_title}', '${bookingData.guest_first_name}', '${bookingData.guest_last_name}', '${bookingData.guest_email}', '${bookingData.guest_address}', '${bookingData.guest_telnum}');
+    `;
 
-            const bookingId = Math.floor(Math.random() * 99999999) + 1;
-            // Proceed to create booking
-            const insertBookingQuery = `
-              INSERT INTO booking (booking_id, checkin_date, checkout_date, guest_id, booking_status, booking_note)
-              VALUES ('${bookingId}','${bookingData.checkin_date}', '${bookingData.checkout_date}', ${guestId}, 'paid', 'Booking confirmed');
-            `;
+      // Execute guest data insertion query
+      connection.query(guestInsertQuery, (guestInsertError, guestInsertResults, guestInsertFields) => {
+        if (guestInsertError) {
+          console.error('Error inserting guest data:', guestInsertError);
+        } else {
+          // Guest data inserted successfully, retrieve the guest_id
 
-            // Execute booking insertion query
-            connection.query(insertBookingQuery, (bookingInsertError, bookingInsertResults, bookingInsertFields) => {
-              if (bookingInsertError) {
-                console.error('Error creating booking:', bookingInsertError);
-              } else {
-                // Booking created successfully, update room_status and booking_room table
-                bookingData.rooms.forEach(room => {
-                  const { room_type_id, num_rooms } = room;
+          const bookingId = Math.floor(Math.random() * 99999999) + 1;
+          // Proceed to create booking
+          const insertBookingQuery = `
+            INSERT INTO booking (booking_id, checkin_date, checkout_date, guest_id, booking_status, booking_note)
+            VALUES ('${bookingId}','${bookingData.checkin_date}', '${bookingData.checkout_date}', ${guestId}, 'paid', 'Booking confirmed');
+          `;
 
-                  // Query room_id based on room_type_id
-                  const selectRoomIdQuery = `
-                    SELECT room_id
-                    FROM room
-                    WHERE room_type_id = ${room_type_id}
-                      AND room_status = 'free'
-                    ORDER BY room_id ASC
-                    LIMIT ${num_rooms};
-                  `;
-                  console.log('Select Room ID Query:', selectRoomIdQuery);
+          // Execute booking insertion query
+          connection.query(insertBookingQuery, (bookingInsertError, bookingInsertResults, bookingInsertFields) => {
+            if (bookingInsertError) {
+              console.error('Error creating booking:', bookingInsertError);
+            } else {
+              // Booking created successfully, update room_status and booking_room table
+              bookingData.rooms.forEach(room => {
+                const { room_type_id, num_rooms } = room;
 
-                  connection.query(selectRoomIdQuery, (selectError, selectResults) => {
-                    if (selectError || selectResults.length === 0) {
-                      console.error(`Error selecting room_id for room_type_id ${room_type_id}:`, selectError);
-                    } else {
-                      const room_id = selectResults[0].room_id;
+                // Query room_id based on room_type_id
+                const selectRoomIdQuery = `
+                  SELECT room_id
+                  FROM room
+                  WHERE room_type_id = ${room_type_id}
+                    AND room_status = 'free'
+                  ORDER BY room_id ASC
+                  LIMIT ${num_rooms};
+                `;
+                console.log('Select Room ID Query:', selectRoomIdQuery);
 
-                
-                      const updateRoomStatusQuery = `
-                        UPDATE room
-                        SET room_status = 'occupined'
-                        WHERE room_id = ${room_id}
-                        LIMIT ${num_rooms};
-                      `;
+                connection.query(selectRoomIdQuery, (selectError, selectResults) => {
+                  if (selectError || selectResults.length === 0) {
+                    console.error(`Error selecting room_id for room_type_id ${room_type_id}:`, selectError);
+                  } else {
+                    const room_id = selectResults[0].room_id;
 
-                      const insertBookingRoomQuery = `
-                        INSERT INTO booking_room (booking_id, room_id)
-                        VALUES (${bookingId}, ${room_id});
-                      `;
 
-                      // Execute room status update and booking_room insertion queries
-                      connection.query(updateRoomStatusQuery, (updateError, updateResults) => {
-                        if (updateError) {
-                          console.error('Error updating room status:', updateError);
-                        } else {
-                          console.log(`Room ${room_id} updated to 'booked'.`);
-                          connection.query(insertBookingRoomQuery, (bookingRoomError, bookingRoomResults) => {
-                            if (bookingRoomError) {
-                              console.error('Error inserting into booking_room:', bookingRoomError);
-                            } else {
-                              console.log('Booking_room entry added successfully.');
-                            }
-                          });
-                        }
-                      });
-                    }
-                  });
+                    const updateRoomStatusQuery = `
+                      UPDATE room
+                      SET room_status = 'occupined'
+                      WHERE room_id = ${room_id}
+                      LIMIT ${num_rooms};
+                    `;
+
+                    const insertBookingRoomQuery = `
+                      INSERT INTO booking_room (booking_id, room_id)
+                      VALUES (${bookingId}, ${room_id});
+                    `;
+
+                    // Execute room status update and booking_room insertion queries
+                    connection.query(updateRoomStatusQuery, (updateError, updateResults) => {
+                      if (updateError) {
+                        console.error('Error updating room status:', updateError);
+                      } else {
+                        console.log(`Room ${room_id} updated to 'booked'.`);
+                        connection.query(insertBookingRoomQuery, (bookingRoomError, bookingRoomResults) => {
+                          if (bookingRoomError) {
+                            console.error('Error inserting into booking_room:', bookingRoomError);
+                          } else {
+                            console.log('Booking_room entry added successfully.');
+                          }
+                        });
+                      }
+                    });
+                  }
                 });
-                console.log('Booking created successfully!');
-              }
-            });
-          }
-        });
-      } else {
-        console.log('Rooms are not available for the specified dates.');
-      }
-    })
-    .catch(error => {
-      console.error('Error checking room availability:', error);
-    });
+              });
+              console.log('Booking created successfully!');
+            }
+          });
+        }
+      });
+    } else {
+      console.log('Some room types are not available for the specified dates.');
+    }
+  } catch (error) {
+    console.error('Error checking room availability:', error);
+  }
 }
 
 // Call the function to create the booking
