f6abb22c4bbd965090e0b8d00307219c700d1075
Lilpoony <tanuthumkingkaew@gmail.com>
2023-11-03 22:52:14 +0700

    change dine page and add facilities page. add picture res 3&4 to fix the resolution

--- Frontend/src/pages/dine.js
+++ Frontend/src/pages/dine.js
@@ -1,25 +1,23 @@
 import React from "react";
 import './dine.css'
-import Restaurant1 from "./image/res1.jpg"
-import Restaurant2 from "./image/res2.jpg"
-import { Link, useNavigate } from "react-router-dom";
-import { useState } from "react";
+import Restaurant3 from "./image/res3.jpg"
+import Restaurant4 from "./image/res4.jpg"
 export default function Dine() {
     return (
       <div className="dine-container">
-        <h2>Dining Options</h2>
+      <h2>Dining Options</h2>
+
+      <div className="restaurant">
+        <h3>Hell's Kitchen</h3>
+        <p>Nigga nae nae</p>
+        <img src={Restaurant3} className="res3 responsive-image" alt="" />
+      </div>
   
-        <div className="restaurant">
-          <h3>Hell's Kitchen</h3>
-          <p>Nigga nae nae</p>
-          <img src={Restaurant1} className="res1" alt="" />
-        </div>
-  
-        <div className="restaurant">
-          <h3>Let Him Cook</h3>
-          <p>Trud trud </p>
-          <img src={Restaurant2} className="res2" alt="" />
-        </div>
+      <div className="restaurant">
+        <h3>Let Him Cook</h3>
+        <p>Trud trud </p>
+        <img src={Restaurant4} className="res4 responsive-image" alt="" />
+      </div>
   
       </div>
     );
