d71da4a2a67d1e404f0e8587722595ce4cae567a
Lilpoony <tanuthumkingkaew@gmail.com>
2023-11-04 12:45:11 +0700

    add more dine.js and dine.css

--- Frontend/src/pages/dine.js
+++ Frontend/src/pages/dine.js
@@ -4,21 +4,50 @@ import Restaurant3 from "./image/res3.jpg"
 import Restaurant4 from "./image/res4.jpg"
 export default function Dine() {
     return (
-      <div className="dine-container">
-      <h2>Dining Options</h2>
+    <div className="dine-container">
+      <h2>View All Dine</h2>
 
-      <div className="restaurant">
-        <h3>Hell's Kitchen</h3>
-        <p>Nigga nae nae</p>
-        <img src={Restaurant3} className="res3 responsive-image" alt="" />
+      <div className="restaurant-container">
+        <div className="restaurant">
+          <div className="image">
+            <img src={Restaurant3} className="res3 responsive-image" alt="" />
+          </div>
+          <div className="details">
+            <h3>Riverside Terrace</h3>
+            <p>Traditional Thai Food.</p>
+            <h4>Lunch</h4>
+            <p>12-3:30pm (Tuesday - Sunday)</p>
+            <h4>Dinner</h4>
+            <p>6:30-11:30pm (Tuesday - Sunday)</p>
+            <h4>Call</h4>
+            <p>+66 (0) 2 659 9000</p>
+            <h4>Email</h4>
+            <p>se-hotel@gmail.com</p>
+          </div>
       </div>
+   
   
       <div className="restaurant">
-        <h3>Let Him Cook</h3>
-        <p>Trud trud </p>
-        <img src={Restaurant4} className="res4 responsive-image" alt="" />
+        <div className="image">
+          <img src={Restaurant4} className="res4 responsive-image" alt="" />
+        </div>
+        <div className="details">
+          <h3>The Verandah</h3>
+          <p>The perfect choice for all-day dining. </p>
+          <h4>Breakfast A la carte</h4>
+          <p>6-11am</p>
+          <h4>Breakfast Buffet</h4>
+          <p>7-10:30am (Monday-Friday)</p>
+          <p>7-11am (Saturday-Sunday)</p>
+          <h4>All Day Dining</h4>
+          <p>10:30am-midnight</p>
+          <h4>Call</h4>
+          <p>+66 (0) 2 659 9000</p>
+          <h4>Email</h4>
+          <p>se-hotel@gmail.com</p>     
+        </div>
+        </div>
       </div>
-  
-      </div>
-    );
+    </div>
+  );
 }
