cdff118aefdca7d4ba116d1adbf5aa9e5aa3d518
Lilpoony <tanuthumkingkaew@gmail.com>
2023-11-11 04:21:57 +0700

    edit roomadmin.js

--- Frontend/src/pages/roomadmin.js
+++ Frontend/src/pages/roomadmin.js
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom"
 import Swal from 'sweetalert2'
 import withReactContent from 'sweetalert2-react-content'
 import './roomadmin.css'
-import 'table.css'
+import './admintable.css'
 
 export default function Roomadmin() {
     const navigate = useNavigate();
