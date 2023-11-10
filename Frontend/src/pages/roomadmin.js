43c567ae2365d1faa6b07b39bf9d482a8704f2fc
Nikkyjung <nikkyjungalbion@gmail.com>
2023-11-10 14:30:50 +0700

    add roomadmin.css and update roomadmin.js

--- Frontend/src/pages/roomadmin.js
+++ Frontend/src/pages/roomadmin.js
@@ -2,12 +2,17 @@ import { React, useState, useEffect } from "react"
 import { useNavigate } from "react-router-dom"
 import Swal from 'sweetalert2'
 import withReactContent from 'sweetalert2-react-content'
+import './roomadmin.css'
 
 export default function Roomadmin() {
     const navigate = useNavigate();
     const MySwal = withReactContent(Swal);
     const [isLoaded, setIsLoaded] = useState(true);
     const [admin, setAdmin] = useState([]);
+    const [isActive, setIsActive] = useState(false);
+    const [selected, setSelected] = useState('All roomtype');
+    const [Search, setSearch] = useState('');
+    const options = ['Deluxe room', 'Luxury room', 'Standard room', 'All roomtype'];
     useEffect(() => {
         const token = localStorage.getItem('token');
         var myHeaders = new Headers();
@@ -40,12 +45,39 @@ export default function Roomadmin() {
         localStorage.removeItem('token')
         navigate('/loginadmin')
       }
+      const data = {selected, Search};
+      console.log(data)
 
     if(isLoaded) return(<div>Loading..</div>)
     return (
         <div>
             <h3>Welcome! {admin.fname} </h3>
             <h4>Room Editing</h4>
+            <div className="dropdown">
+              <div className="dropdown-btn" onClick={(e) =>
+              setIsActive(!isActive)}>
+                {selected}
+                <span className="dropdown-span">v</span>
+              </div>
+              {isActive && (
+                <div className="dropdown-content">
+                  { options.map((options) =>
+                    <div
+                      onClick={(e) => {
+                        setSelected(options);
+                        setIsActive(false);
+                      }}
+                      className="dropdown-item">
+                        {options}
+                  </div>
+                  )}
+                </div>
+              )}
+            </div>
+            <div className="seachbar">
+              <p>Search<input type="text" placeholder="Booking ID" onChange={e => 
+              setSearch(e.target.value)} /></p>
+            </div>
             <div><button onClick={logout}>Logout</button></div>
         </div>
     )
