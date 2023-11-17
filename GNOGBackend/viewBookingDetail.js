9f9a9c8c8b1e3c88b396e9979ccf3afea7f561a4
GNOGGONG <peerawat291@gmail.com>
2023-11-18 01:58:58 +0700

    add room_status

--- GNOGBackend/viewBookingDetail.js
+++ GNOGBackend/viewBookingDetail.js
@@ -16,53 +16,67 @@ const pool = mysql.createPool({
 });
 
 async function viewBookingDetail(booking_id) {
-    try {
-      const connection = await pool.getConnection();
-  
-      const [results, fields] = await connection.execute(`
-        SELECT
-          b.booking_id,
-          DATE_FORMAT(b.checkin_date, '%Y-%m-%d') AS checkin_date,
-          DATE_FORMAT(b.checkout_date, '%Y-%m-%d') AS checkout_date,
-          b.booking_status,
-          b.booking_note,
-          g.guest_title,
-          g.guest_first_name,
-          g.guest_middle_name,
-          g.guest_last_name,
-          g.guest_telnum,
-          g.guest_email,
-          g.guest_address,
-          GROUP_CONCAT(br.room_id) AS room_ids,
-          GROUP_CONCAT(rt.room_type_name) AS room_type_names
-        FROM booking b
-        JOIN guest g ON b.guest_id = g.guest_id
-        LEFT JOIN booking_room br ON b.booking_id = br.booking_id
-        LEFT JOIN room r ON br.room_id = r.room_id
-        LEFT JOIN room_type rt ON r.room_type_id = rt.room_type_id
-        WHERE b.booking_id = ?
-        GROUP BY b.booking_id, b.checkin_date, b.checkout_date, b.booking_status, b.booking_note,
-          g.guest_title, g.guest_first_name, g.guest_middle_name, g.guest_last_name,
-          g.guest_telnum, g.guest_email, g.guest_address;
-      `, [booking_id]);
-  
-      connection.release();
-  
-      if (results.length === 0) {
-        return { success: false, message: 'Booking not found.' };
-      }
-  
-      const bookingDetails = results[0];
-      bookingDetails.room_ids = bookingDetails.room_ids.split(',').map(Number); // Convert to array of numbers
-      bookingDetails.room_type_names = bookingDetails.room_type_names.split(','); // Convert to array of strings
-  
-      return { success: true, bookingDetails };
-    } catch (error) {
-      console.error('Error fetching booking details:', error);
-      throw error;
+  try {
+    const connection = await pool.getConnection();
+
+    const [results, fields] = await connection.execute(`
+      SELECT
+        b.booking_id,
+        DATE_FORMAT(b.checkin_date, '%Y-%m-%d') AS checkin_date,
+        DATE_FORMAT(b.checkout_date, '%Y-%m-%d') AS checkout_date,
+        b.booking_status,
+        b.booking_note,
+        g.guest_title,
+        g.guest_first_name,
+        g.guest_middle_name,
+        g.guest_last_name,
+        g.guest_telnum,
+        g.guest_email,
+        g.guest_address,
+        br.room_id AS room_id,
+        rt.room_type_name AS room_type_name,
+        r.room_status AS room_status
+      FROM booking b
+      JOIN guest g ON b.guest_id = g.guest_id
+      LEFT JOIN booking_room br ON b.booking_id = br.booking_id
+      LEFT JOIN room r ON br.room_id = r.room_id
+      LEFT JOIN room_type rt ON r.room_type_id = rt.room_type_id
+      WHERE b.booking_id = ?
+    `, [booking_id]);
+
+    connection.release();
+
+    if (results.length === 0) {
+      return { success: false, message: 'Booking not found.' };
     }
+
+    const bookingDetails = {
+      booking_id: results[0].booking_id,
+      checkin_date: results[0].checkin_date,
+      checkout_date: results[0].checkout_date,
+      booking_status: results[0].booking_status,
+      booking_note: results[0].booking_note,
+      guest_title: results[0].guest_title,
+      guest_first_name: results[0].guest_first_name,
+      guest_middle_name: results[0].guest_middle_name,
+      guest_last_name: results[0].guest_last_name,
+      guest_telnum: results[0].guest_telnum,
+      guest_email: results[0].guest_email,
+      guest_address: results[0].guest_address,
+      rooms: results.map((result) => ({
+        room_id: result.room_id,
+        room_type_name: result.room_type_name,
+        room_status: result.room_status,
+      })),
+    };
+
+    return { success: true, bookingDetails };
+  } catch (error) {
+    console.error('Error fetching booking details:', error);
+    throw error;
   }
-  
-  module.exports = {
-    viewBookingDetail: viewBookingDetail,
-  };
+}
+
+module.exports = {
+  viewBookingDetail: viewBookingDetail,
+};
