2a33117b0da71127402b6c629d4ab9c2de665271
Lilpoony <tanuthumkingkaew@gmail.com>
2023-11-19 03:26:05 +0700

    add picture. Edit surrounding.js and facility.js

--- Frontend/src/pages/facilities.js
+++ Frontend/src/pages/facilities.js
@@ -1,25 +1,85 @@
 import React from "react";
-import './facilities.css'
+import './dine.css'
+import Swimming from "./image/swim.png"
+import Fitness from "./image/fitness.png"
+import Spa from "./image/spa.png"
+import Meeting from "./image/meeting.png"
 
-export default function Facilities() {
+export default function Dine() {
     return (
-        <div>
-            <div className="facilities-container">
-            <div className="facilities">
-                <h2>Our Facilities</h2>
-                    <li>Swimming Pool</li>
-                    <li>Fitness Center</li>
-                    <li>Spa & Wellness Center</li>
-                    <li>Meeting Rooms</li>
-            </div>
-            <div className="services">
-                <h2>Our Services</h2>
-                    <li>Room Service</li>
-                    <li>Concierge Service</li>
-                    <li>24/7 Front Desk</li>
-                    <li>Laundry Service</li>
-            </div>
-            </div>
+    <div className="dine-container">
+      <h2>Our facilities & Services</h2>
+
+      <div className="restaurant-container">
+        <div className="restaurant">
+          <div className="image">
+            <img src={Swimming} className="swim responsive-image" alt="" />
+          </div>
+          <div className="details">
+            <h3>Swimming Pool</h3>
+            <p>Enjoy a refreshing dip in our pool.</p>
+            <h4>Opening Hours</h4>
+            <p>Monday - Sunday</p>
+            <p>8am - 8pm</p>
+            <h4>Contact</h4>
+            <p>+66 (0) 2 659 9000</p>
+            <h4>Email</h4>
+            <p>se-hotel@gmail.com</p>
+          </div>
+      </div>
+   
+  
+      <div className="restaurant">
+        <div className="image">
+          <img src={Fitness} className="fitness responsive-image" alt="" />
         </div>
-    );
+        <div className="details">
+            <h3>Fitness Center</h3>
+            <p>Stay fit with our state-of-the-art fitness facilities.</p>
+            <h4>Opening Hours</h4>
+            <p>Monday - Sunday</p>
+            <p>6am - 10pm</p>
+            <h4>Contact</h4>
+            <p>+66 (0) 2 659 9000</p>
+            <h4>Email</h4>
+            <p>se-hotel@gmail.com</p> 
+        </div>
+        </div>
+
+        <div className="restaurant">
+        <div className="image">
+          <img src={Spa} className="spa responsive-image" alt="" />
+        </div>
+        <div className="details">
+            <h3>Spa & Wellness Center</h3>
+            <p>Indulge in relaxation and rejuvenation at our spa.</p>
+            <h4>Opening Hours</h4>
+            <p>Tuesday - Sunday</p>
+            <p>10am - 8pm</p>
+            <h4>Contact</h4>
+            <p>+66 (0) 2 659 9000</p>
+            <h4>Email</h4>
+            <p>se-hotel@gmail.com</p>
+        </div>
+        </div>
+
+        <div className="restaurant">
+        <div className="image">
+          <img src={Meeting} className="meeting responsive-image" alt="" />
+        </div>
+        <div className="details">
+            <h3>Meeting Rooms</h3>
+            <p>Host your events and meetings in our versatile meeting spaces.</p>
+            <h4>Availability</h4>
+            <p>By reservation</p>
+            <h4>Contact</h4>
+            <p>+66 (0) 2 659 9000</p>
+            <h4>Email</h4>
+            <p>se-hotel@gmail.com</p>
+        </div>
+        </div>
+
+    </div>
+    </div>
+  );
 }
