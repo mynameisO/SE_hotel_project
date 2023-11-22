728bfc8e7f538016eafe70880d68687fcf262b00
Nikkyjung <nikkyjungalbion@gmail.com>
2023-11-23 00:28:17 +0700

    update booking page

--- Frontend/src/pages/booking.js
+++ Frontend/src/pages/booking.js
@@ -1,17 +1,34 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./booking.css"
+import Swal from 'sweetalert2'
+import withReactContent from 'sweetalert2-react-content'
 
 export default function Booking() {
     const [firstdate, setFirstDate] = useState('');
     const [enddate, setEndDate] = useState('');
     const navigate = useNavigate();
     const isProcess = 0;
+    const MySwal = withReactContent(Swal);
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        const dateBooking = {firstdate, enddate, isProcess};
-        navigate('/bookingroom', {replace: true, state:{dateBooking}});
+        fetch(`http://${process.env.REACT_APP_BACKEND_IP}/api/room/room_available?checkin_date=${firstdate}&checkout_date=${enddate}`)
+        .then(response => response.json())
+          .then(result => {
+            if(result.error){
+                MySwal.fire({
+                    html : <i>{result.error}</i>,
+                    icon : 'error'
+                }).then((value) => {
+                    navigate('/booking')
+                })
+            }else {
+                const dateBooking = {firstdate, enddate, isProcess};
+                navigate('/bookingroom', {replace: true, state:{dateBooking}});
+            }
+          })
+          .catch(error => console.log('error', error));
     }
     return (
         <bodylhh>
